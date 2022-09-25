456213c345ed2c1a6e148df15f9342cdbd4a7786
bxfidge <thore.mueller07@gmail.com>
2022-09-25 18:24:28 +0200

    Initial commit

---
+++ System/src/main/java/net/nightpixel/cyberpunk/system/SystemMain.java
@@ -0,0 +1,21 @@
+package net.nightpixel.cyberpunk.system;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SystemMain extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
